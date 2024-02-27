62b7dafd21a43d2c9c55d52c4de3ea3ea0b93808
Bartosz Dulęba <kontakt@bartoszduleba.pl>
2024-02-27 01:25:42 +0100

    Stan na 27.02.2026 godz. 1:25

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 const Homepage = () => {
-  return <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+  return <div className='h-full flex flex-col gap-8 lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
     {/* IMAGE CONTAINER */}
     <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
-      <Image src='/hero.png' alt='' fill className='object-contain' />
+      <Image src='/apps.webp' alt='' fill className='object-contain' />
     </div>
     {/* TEXT CONTAINER */}
     <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
